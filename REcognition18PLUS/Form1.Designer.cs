5e64c2c12363072e33d82bd811009ec94e858f4d
Witcher-MTM <phantomit228@gmail.com>
2022-05-02 22:39:58 +0300

    Add project files.

---
+++ REcognition18PLUS/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace REcognition18PLUS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img_add_btn = new System.Windows.Forms.Button();
+            this.clearpicture_btn = new System.Windows.Forms.Button();
+            this.WTF = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img_add_btn
+            // 
+            this.img_add_btn.Location = new System.Drawing.Point(12, 12);
+            this.img_add_btn.Name = "img_add_btn";
+            this.img_add_btn.Size = new System.Drawing.Size(75, 83);
+            this.img_add_btn.TabIndex = 0;
+            this.img_add_btn.Text = "Upload Image";
+            this.img_add_btn.UseVisualStyleBackColor = true;
+            this.img_add_btn.Click += new System.EventHandler(this.img_add_btn_Click);
+            // 
+            // clearpicture_btn
+            // 
+            this.clearpicture_btn.Location = new System.Drawing.Point(12, 173);
+            this.clearpicture_btn.Name = "clearpicture_btn";
+            this.clearpicture_btn.Size = new System.Drawing.Size(75, 90);
+            this.clearpicture_btn.TabIndex = 1;
+            this.clearpicture_btn.Text = "Clear";
+            this.clearpicture_btn.UseVisualStyleBackColor = true;
+            this.clearpicture_btn.Click += new System.EventHandler(this.clearpicture_btn_Click);
+            // 
+            // WTF
+            // 
+            this.WTF.Location = new System.Drawing.Point(12, 357);
+            this.WTF.Name = "WTF";
+            this.WTF.Size = new System.Drawing.Size(75, 81);
+            this.WTF.TabIndex = 2;
+            this.WTF.Text = "button3";
+            this.WTF.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(93, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(695, 435);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.WTF);
+            this.Controls.Add(this.clearpicture_btn);
+            this.Controls.Add(this.img_add_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button img_add_btn;
+        private System.Windows.Forms.Button clearpicture_btn;
+        private System.Windows.Forms.Button WTF;
+        public System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ REcognition18PLUS/Form1.cs
@@ -0,0 +1,123 @@
+﻿using Amazon.Rekognition.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace REcognition18PLUS
+{
+    public partial class Form1 : Form
+    {
+        public static string res { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            res = string.Empty;
+        }
+
+        private void img_add_btn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                file.Filter = "png|*.png|jpeg|*.jpeg";
+
+                
+               if(file.ShowDialog() == DialogResult.OK)
+               {
+                    this.pictureBox1.Image = System.Drawing.Image.FromFile(file.FileName);
+                    this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                    Main(file.FileName);
+               }
+
+            }
+            
+        }
+        public async Task Main(string pathImage)
+        {
+            string photo = pathImage;
+
+            var awsCredentials = new Amazon.Runtime.BasicAWSCredentials("AKIA2SEVNCMGZ3GCJV5X", "zYAzTTLzYYGrr/tZ1E4Vw4MRSF1UA+uw47Fqn6yi");
+            var rekognitionClient = new Amazon.Rekognition.AmazonRekognitionClient(awsCreden‌tials, Amazon.RegionEndpoint.USEast1);
+
+            var image = new Amazon.Rekognition.Model.Image();
+            try
+            {
+                using (var fs = new FileStream(photo, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = null;
+                    data = new byte[fs.Length];
+                    fs.Read(data, 0, (int)fs.Length);
+                    image.Bytes = new MemoryStream(data);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to load file " + photo);
+                return;
+            }
+
+            int height;
+            int width;
+
+            // Used to extract original photo width/height
+            using (var imageBitmap = new Bitmap(photo))
+            {
+                height = imageBitmap.Height;
+                width = imageBitmap.Width;
+            }
+
+            try
+            {
+                var detectFacesRequest = new DetectFacesRequest()
+                {
+                    Image = image,
+                    Attributes = new List<string>() { "ALL" },
+                };
+
+                DetectFacesResponse detectFacesResponse = await rekognitionClient.DetectFacesAsync(detectFacesRequest);
+                detectFacesResponse.FaceDetails.ForEach(face =>
+                {
+                  
+                    ShowBoundingBoxPositions(
+                        height,
+                        width,
+                        face.BoundingBox,
+                        face.AgeRange);
+                   
+                });
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        
+        public void ShowBoundingBoxPositions(int imageHeight, int imageWidth, BoundingBox box, AgeRange age)
+        {
+           
+            float top = (box.Top * pictureBox1.Height);
+            float left = (box.Left * pictureBox1.Width);
+            float right = pictureBox1.Width * box.Width;
+            float bottom = pictureBox1.Height * box.Height;
+             MessageBox.Show("Drawing rects");
+
+            Graphics graphic_picture = this.pictureBox1.CreateGraphics();
+            graphic_picture.DrawRectangle(new Pen(Color.Aqua,3f),left,top,right,bottom);
+            graphic_picture.DrawString($"{age.High.ToString()}-Age",new Font(FontFamily.GenericSansSerif,15f),Brushes.GreenYellow,new PointF(left,right));
+        }
+
+        private void clearpicture_btn_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = null;
+        }
+    }
+}
